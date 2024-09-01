e9c6121210e3ba3a89419adda6154f4862354b2e
kobayashi70 <70929nao@gmail.com>
2024-09-02 00:54:48 +0900

    エラー解消0902

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import quizReducer from "./quizSlice";
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     quiz: quizReducer,
   },
